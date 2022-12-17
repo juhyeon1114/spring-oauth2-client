01519cbe4db2533f4bc8b0ff8e1da296612cb850
juhyeon <juhyeon1114@gmail.com>
2022-12-17 14:31:01 +0900

    init

---
+++ src/main/java/security/springoauth2client/IndexController.java
@@ -0,0 +1,14 @@
+package security.springoauth2client;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String Index() {
+        return "index";
+    }
+
+}
