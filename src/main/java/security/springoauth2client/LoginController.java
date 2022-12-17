2522a288df9c1721ad7ef7e04a3ae103d9cfcea4
juhyeon <juhyeon1114@gmail.com>
2022-12-17 17:03:48 +0900

    login config

---
+++ src/main/java/security/springoauth2client/LoginController.java
@@ -0,0 +1,15 @@
+package security.springoauth2client;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+
+
+}
---
+++ src/main/java/security/springoauth2client/OAuth2ClientConfig.java
@@ -0,0 +1,24 @@
+package security.springoauth2client;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class OAuth2ClientConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/loginPage").permitAll()
+                .anyRequest()
+                .authenticated();
+
+        http.oauth2Login(config -> config.loginPage("/loginPage"));
+
+        return http.build();
+    }
+
+}
