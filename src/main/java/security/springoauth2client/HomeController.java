7df1c1e7643457ad272ec207004f38e554d7587a
juhyeon <juhyeon1114@gmail.com>
2022-12-20 17:00:16 +0900

    어노테이션으로 AuthorizedClient가져오기

--- src/main/java/security/springoauth2client/HomeController.java
+++ src/main/java/security/springoauth2client/HomeController.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,16 +15,17 @@ public class HomeController {
 
     private final OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
 
-
     @GetMapping("/home")
-    public String home(Model model, Authentication authentication) {
-
-        OAuth2AuthorizedClient authorizedClient = oAuth2AuthorizedClientService.loadAuthorizedClient("keycloak", authentication.getName());
+    public String home(
+            Model model,
+           @RegisteredOAuth2AuthorizedClient("keycloak") OAuth2AuthorizedClient authorizedClient,
+   ) {
 
         model.addAttribute("AccessToken", authorizedClient.getAccessToken());
         model.addAttribute("RefreshToken", authorizedClient.getRefreshToken());
 
         return "home";
+
     }
 
 }
--- src/main/java/security/springoauth2client/controller/LoginController.java
+++ src/main/java/security/springoauth2client/controller/LoginController.java
@@ -4,15 +4,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2Token;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +29,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.Clock;
 import java.time.Duration;
+import java.util.Set;
 
 @Controller
 @RequiredArgsConstructor
@@ -30,10 +40,38 @@ public class LoginController {
     private Duration clockSkew = Duration.ofSeconds(60);
     private Clock clock = Clock.systemUTC();
 
+    /**
+     * 어노테이션으로 AuthorizedClient가져오기
+     */
+    @GetMapping("/v2/oauth2Login")
+    public String oauth2LoginV2(
+            @RegisteredOAuth2AuthorizedClient("keycloak") OAuth2AuthorizedClient authorizedClient,
+            Model model
+    ) {
+        if (authorizedClient != null) {
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
+            ClientRegistration clientRegistration = authorizedClient.getClientRegistration();
+            OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+            OAuth2UserRequest oAuth2UserRequest = new OAuth2UserRequest(clientRegistration, accessToken);
+            OAuth2User oAuth2User = oAuth2UserService.loadUser(oAuth2UserRequest);
 
+            SimpleAuthorityMapper authorityMapper = new SimpleAuthorityMapper();
+            authorityMapper.setPrefix("SYSTEM_");
+            Set<GrantedAuthority> grantedAuthorities = authorityMapper.mapAuthorities(oAuth2User.getAuthorities());
+            OAuth2AuthenticationToken oAuth2AuthenticationToken = new OAuth2AuthenticationToken(oAuth2User, grantedAuthorities, clientRegistration.getRegistrationId());
+            SecurityContextHolder.getContext().setAuthentication(oAuth2AuthenticationToken);
+
+            model.addAttribute("oAuth2AuthenticationToken", oAuth2AuthenticationToken);
+        }
+        model.addAttribute("authorizedClient", authorizedClient.getAccessToken().getTokenValue());
+
+        return "home";
+    }
+    
+    
     @GetMapping("/oauth2Login")
     public String oauth2Login(HttpServletRequest request, HttpServletResponse response, Model model) {
-
+        
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
@@ -62,8 +100,8 @@ public class LoginController {
         // refresh 토큰으로 재발급 받기
         if (
                 authorizedClient != null &&
-                hasTokenExpired(authorizedClient.getAccessToken()) &&
-                authorizedClient.getRefreshToken() != null
+                        hasTokenExpired(authorizedClient.getAccessToken()) &&
+                        authorizedClient.getRefreshToken() != null
         ) {
 
             ClientRegistration clientRegistration = ClientRegistration
